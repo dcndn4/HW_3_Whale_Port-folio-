f63686a97ddb602c95d1f8df2125208eb3326857
dcndn4 <stokes@iphouse.com>
2021-09-28 14:30:18 -0500

    different inputs cleaned different ways

--- Whale_off_Portflio.py
+++ Whale_off_Portflio.py
@@ -39,9 +39,31 @@ print(algo_returns.head())
 #file_path = Path="C:/Users/CS_Knit_tinK_SC/Documents/My Data Sources/loans.csv"
 #csv_path = Path="C:/Users/CS_Knit_tinK_SC/Documents/My Data Sources/goog_google_finance.csv"
 csv_path_s = "C:/Users/CS_Knit_tinK_SC/Documents/My Data Sources/Whale/sp500_history.csv"
-sp500_history = pd.read_csv(
-    csv_path_s, parse_dates=True, index_col="Date", infer_datetime_format=True
-)
+#sp500_history = pd.read_csv(
+#csv_path_s, parse_dates=True, index_col="Date", infer_datetime_format=True
+#)
+sp500_history = pd.read_csv(csv_path_s)
 print(sp500_history.head())
 
 #%%
+
+# Check Data Types
+print(sp500_history.dtypes)
+
+#%%
+
+#Set index as date field
+
+#sp500_history.set_index(sp500_history['Date'], inplace=True)
+#print(sp500_history.head())
+
+sp500_history.set_index(pd.to_datetime(sp500_history['Date'], infer_datetime_format=True), inplace=True)
+print(sp500_history.head())
+
+#%%
+# Drop the extra date column
+sp500_history.drop(columns=['Date'], inplace=True)
+print(sp500_history.head())
+
+#%%
+
